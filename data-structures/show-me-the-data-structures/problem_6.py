d0ffa405399658f6e66944825351b9db2a4d34a2
MinervaRose <39020690+MinervaRose@users.noreply.github.com>
2025-04-22 18:21:58 +0200

    Update problem_6.py

--- data-structures/show-me-the-data-structures/problem_6.py
+++ data-structures/show-me-the-data-structures/problem_6.py
@@ -1,82 +1,26 @@
 from typing import Optional
 
 class Node:
-    """
-    A class to represent a node in a linked list.
-
-    Attributes:
-    -----------
-    value : int
-        The value stored in the node.
-    next : Optional[Node]
-        The reference to the next node in the linked list.
-    """
-
     def __init__(self, value: int) -> None:
-        """
-        Constructs all the necessary attributes for the Node object.
-
-        Parameters:
-        -----------
-        value : int
-            The value to be stored in the node.
-        """
         self.value: int = value
         self.next: Optional[Node] = None
 
     def __repr__(self) -> str:
-        """
-        Return a string representation of the node.
-
-        Returns:
-        --------
-        str
-            A string representation of the node's value.
-        """
         return str(self.value)
 
-
 class LinkedList:
-    """
-    A class to represent a singly linked list.
-
-    Attributes:
-    -----------
-    head : Optional[Node]
-        The head node of the linked list.
-    """
-
     def __init__(self) -> None:
-        """
-        Constructs all the necessary attributes for the LinkedList object.
-        """
         self.head: Optional[Node] = None
 
     def __str__(self) -> str:
-        """
-        Return a string representation of the linked list.
-
-        Returns:
-        --------
-        str
-            A string representation of the linked list, with nodes separated by " -> ".
-        """
         cur_head: Optional[Node] = self.head
         out_string: str = ""
         while cur_head:
             out_string += str(cur_head.value) + " -> "
             cur_head = cur_head.next
-        return out_string
+        return out_string.strip(" -> ")
 
     def append(self, value: int) -> None:
-        """
-        Append a new node with the given value to the end of the linked list.
-
-        Parameters:
-        -----------
-        value : int
-            The value to be stored in the new node.
-        """
         if self.head is None:
             self.head = Node(value)
             return
@@ -84,26 +28,15 @@ class LinkedList:
         node: Node = self.head
         while node.next:
             node = node.next
-
         node.next = Node(value)
 
     def size(self) -> int:
-        """
-        Calculate the size (number of nodes) of the linked list.
-
-        Returns:
-        --------
-        int
-            The number of nodes in the linked list.
-        """
         size: int = 0
         node: Optional[Node] = self.head
         while node:
             size += 1
             node = node.next
-
         return size
-
 
 def union(llist_1: LinkedList, llist_2: LinkedList) -> LinkedList:
     result = LinkedList()
@@ -111,95 +44,75 @@ def union(llist_1: LinkedList, llist_2: LinkedList) -> LinkedList:
 
     current = llist_1.head
     while current:
-        seen.add(current.value)
+        if current.value not in seen:
+            seen.add(current.value)
+            result.append(current.value)
         current = current.next
 
     current = llist_2.head
     while current:
-        seen.add(current.value)
+        if current.value not in seen:
+            seen.add(current.value)
+            result.append(current.value)
         current = current.next
 
-    for value in seen:
-        result.append(value)
-
     return result
-
 
 def intersection(llist_1: LinkedList, llist_2: LinkedList) -> LinkedList:
     result = LinkedList()
     set1 = set()
-    set2 = set()
 
     current = llist_1.head
     while current:
         set1.add(current.value)
         current = current.next
 
+    already_added = set()
     current = llist_2.head
     while current:
-        set2.add(current.value)
+        if current.value in set1 and current.value not in already_added:
+            result.append(current.value)
+            already_added.add(current.value)
         current = current.next
-
-    common = set1.intersection(set2)
-
-    for value in common:
-        result.append(value)
 
     return result
 
-
 if __name__ == "__main__":
-    ## Test case 1
+    # Test Case 1
+    print("Test Case 1")
     linked_list_1 = LinkedList()
     linked_list_2 = LinkedList()
-
-    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
-    element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
-    
-    for i in element_1:
+    for i in [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]:
         linked_list_1.append(i)
-
-    for i in element_2:
+    for i in [6, 32, 4, 9, 6, 1, 11, 21, 1]:
         linked_list_2.append(i)
+    print("Union:", union(linked_list_1, linked_list_2))
+    print("Intersection:", intersection(linked_list_1, linked_list_2))
 
-    print("Test Case 1:")
-    print("Union:", union(linked_list_1, linked_list_2)) # Expected: 1, 2, 3, 4, 6, 9, 11, 21, 32, 35, 65
-    print("Intersection:", intersection(linked_list_1, linked_list_2)) # Expected: 4, 6, 21
-
-    ## Test case 2
+    # Test Case 2: No common elements
+    print("\nTest Case 2")
     linked_list_3 = LinkedList()
     linked_list_4 = LinkedList()
-
-    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
-    element_2 = [1, 7, 8, 9, 11, 21, 1]
-
-    for i in element_1:
+    for i in [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]:
         linked_list_3.append(i)
-
-    for i in element_2:
+    for i in [1, 7, 8, 9, 11, 21, 1]:
         linked_list_4.append(i)
+    print("Union:", union(linked_list_3, linked_list_4))
+    print("Intersection:", intersection(linked_list_3, linked_list_4))  # Expected: empty
 
-    print("\nTest Case 2:")
-    print("Union:", union(linked_list_3, linked_list_4)) # Expected: 1, 2, 3, 4, 6, 7, 8, 9, 11, 21, 23, 35, 65
-    print("Intersection:", intersection(linked_list_3, linked_list_4)) # Expected: empty
-
-    ## Test case 3
-    print("\nTest Case 3: One list is empty")
+    # Test Case 3: One list empty
+    print("\nTest Case 3")
     linked_list_5 = LinkedList()
     linked_list_6 = LinkedList()
-
     for i in [1, 2, 3]:
         linked_list_5.append(i)
+    # linked_list_6 remains empty
+    print("Union:", union(linked_list_5, linked_list_6))  # Expected: 1 -> 2 -> 3
+    print("Intersection:", intersection(linked_list_5, linked_list_6))  # Expected: empty
 
-    # linked_list_6 is empty
-
-    print("Union:", union(linked_list_5, linked_list_6))         # Expected: 1, 2, 3
-    print("Intersection:", intersection(linked_list_5, linked_list_6)) # Expected: empty
-
-    ## Test case 4
-    print("\nTest Case 4: Both lists are empty")
+    # Test Case 4: Both lists empty
+    print("\nTest Case 4")
     linked_list_7 = LinkedList()
     linked_list_8 = LinkedList()
-
-    print("Union:", union(linked_list_7, linked_list_8))         # Expected: empty
-    print("Intersection:", intersection(linked_list_7, linked_list_8)) # Expected: empty
+    print("Union:", union(linked_list_7, linked_list_8))  # Expected: empty
+    print("Intersection:", intersection(linked_list_7, linked_list_8))  # Expected: empty
